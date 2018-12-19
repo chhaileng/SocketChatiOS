fda0c3f79354138d858fb73d682657252cd75e88
Chhaileng Peng <chhailengpeng@gmail.com>
2018-12-19 11:50:27 +0700

    Initial Commit

---
+++ SocketChatiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocketChatiOS
+//
+//  Created by Chhaileng Peng on 12/19/18.
+//  Copyright © 2018 Chhaileng Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
